bd9f5b204e8188baf2da0b743e67086909eb2d9a
yanxin152133 <yanxin152133@gmail.com>
2020-02-11 17:05:19 +0800

    Linux+docker搭建饥荒服务器

--- 更新脚本/updateDST.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-steamcmd_dir="$HOME/steamcmd"
-## 检查并更新更新游戏版本
-cd "$steamcmd_dir"
-./steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir ../dontstarvetogether_dedicated_server +app_update 343050 validate +quit
-
